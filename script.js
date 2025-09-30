91d4a962ca4d974017a249e487001525e8703bdd
LightSpark75002 <119753700+RuslanBelarus@users.noreply.github.com>
2025-09-30 19:25:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+var ipList = new Array();
+ipList.push('37.213.10.223'); //37.213.10.223
+
+var verifPressed = false;
+
+var betaMod = true;
+
+
+function videoLoader(val) {
+	var str = "";
+	for (var i = 1; i < val; i++) {
+		str = str + '<video src="source/video' + i + '.mp4" controls></video>';
+	}
+	return str
+}
+
+function getId(id) {
+	alert(id);
+}
+
+function pageLoad() {
+	document.getElementById('wml').remove();
+	document.getElementById('wmd').remove();
+	document.getElementById('wmb').remove();
+	document.getElementById('label').textContent = 'Заметки котакбаса';
+	document.getElementById('sublabel').textContent = 'уникальные видео';
+	document.getElementById('chapter').textContent = 'Важно';
+	document.getElementById('chapter').style.color = 'red';
+	document.getElementById('describe').textContent = 'Снятие и распростаранение данных видео автор данной платформы не поддерживает. Как же, как и их происходящее. Видео сняты и выложены с разрешения участвующих в них лиц.';
+	document.getElementById('dsc').innerHTML = 'Операторы: <button onclick=\'alert("Discord: 0lalka0")\'>Dreamer</button> <button onclick=\'alert("Discord: dr.dexideb")\'>Lixpan4ik</button> <button onclick=\'alert("Discord: alexvern324")\'>_AlexMain_</button> Создатель: <button onclick=\'alert("Discord: dr.dexideb")\'>Lixpan4ik</button>';
+	document.getElementsByClassName('hr')[0].innerHTML = '<hr>';
+	document.getElementsByClassName('hr')[1].innerHTML = '<hr>';
+	document.getElementsByClassName('hr')[2].innerHTML = '<hr>';
+	document.getElementById('videos').innerHTML = videoLoader(8);
+
+}
+
+function betaIp() {
+	document.getElementById('wml').textContent = 'IP verifing';
+	document.getElementById('wmd').textContent = 'Сайт в разработке';
+	document.getElementById('wmd').style.color = 'darkred';
+	document.getElementById('wmb').remove();
+	console.log(document.getElementById('wmd').textContent);
+}
+
+function suffecultIp() {
+	document.getElementById('wml').textContent = 'IP verifing';
+	document.getElementById('wmd').textContent = 'Ваш IP подтвержден';
+	document.getElementById('wmd').style.color = 'green';
+	document.getElementById('wmb').textContent = 'Загрузить страницу';
+	console.log(document.getElementById('wmd').textContent);
+}
+
+function cancelIp() {
+	document.getElementById('wml').textContent = 'IP verifing';
+	document.getElementById('wmd').textContent = 'Ваш IP не подтвержден';
+	document.getElementById('wmd').style.color = 'red';
+	document.getElementById('wmb').remove();
+	console.log(document.getElementById('wmd').textContent);
+}
+
+function verifIp2(ip) {
+	var ver = false;
+	for (var i = 0; i <= ipList.length; i++) {
+		if (ip == ipList[i]) {
+			suffecultIp();
+			ver = true;
+		} else {
+			if (!ver) {
+				cancelIp();
+			}
+		}
+	}
+}
+
+function verifIp() {
+	fetch('https://api.ipify.org?format=json')
+		.then(response => response.json())
+		.then(data => {
+			verifIp2(data.ip);
+		})
+		.catch(error => {
+			cancelIp();
+		});
+}
+
+function buttonBuffer() {
+	if (!betaMod) {
+		if (verifPressed) {
+			pageLoad();
+		} else {
+			verifPressed = true;
+			verifIp();
+		}
+	} else {
+		betaIp();
+	}
+}
